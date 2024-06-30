ea13cdab67f59464e90819af716d82f469f3c1d8
navjotsingh2608 <navjot@argenelec.com>
2024-06-30 02:36:22 -0400

    add status bar for the app

---
+++ screens/RestaurantScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, SafeAreaView } from "react-native";
+
+const RestaurantScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Restaurant Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default RestaurantScreen;
